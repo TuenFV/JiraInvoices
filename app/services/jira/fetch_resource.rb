14ba7757331404e2a9686a4d06b471f5d1e53743
Duyet Nguyen <duyet.vn@gmail.com>
2022-06-02 09:24:09 +0700

    JI-4/fetch jira resources.

---
+++ app/services/jira/fetch_resource.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'uri'
+
+module Jira
+  class FetchResource
+    def initialize(resource)
+      # https://tuenfv.atlassian.net/rest/api/3/project
+      @url = "https://tuenfv.atlassian.net/rest/api/3/#{resource}"
+    end
+
+    def perform
+      username = ENV['JIRA_USERNAME']
+      password = ENV['JIRA_PASSWORD']
+      uri = URI.parse(@url)
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth(username, password)
+      request.content_type = 'application/json'
+
+      req_options = {
+        use_ssl: uri.scheme == 'https'
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
