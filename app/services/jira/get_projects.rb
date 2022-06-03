53cf67197a401180e018dc4d8e581f1f2ecdb9e0
LoKi <sand5088@gmail.com>
2022-06-03 15:34:16 +0700

    get project service

---
+++ app/services/jira/get_projects.rb
@@ -0,0 +1,22 @@
+module Jira
+  class GetProjects
+    def initialize
+      jira_projects = Jira::FetchResource.new('project').perform
+      organization_jira = jira_projects[0][:self].split('/')[2].split('.')[0]
+      jira_projects.each do |jira_project|
+        project = Project.find_or_initialize_by(jira_project_id: jira_project[:id])
+
+        project.name = jira_project[:name]
+        project.code = jira_project[:key]
+        project.organization_id = Organization.find_by_domain(organization_jira).id
+
+        if project.save
+          puts 'Get Projects OK!'
+        else
+          puts 'Get Projects Fails'
+        end
+        puts "------------"
+      end
+    end
+  end
+end
