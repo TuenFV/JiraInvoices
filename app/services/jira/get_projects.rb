a0e2c8a6bea61a0c830fce0771402b4b5540d4a7
LoKi <sand5088@gmail.com>
2022-06-04 16:37:50 +0700

    fix after comment with @organization.domain

--- app/services/jira/get_projects.rb
+++ app/services/jira/get_projects.rb
@@ -1,14 +1,14 @@
 module Jira
   class GetProjects
     def initialize(organization)
-      @organization = organization
+      @organization = Organization.find_or_create_by(domain: organization)
     end
 
     def perform
-      jira_projects = Jira::FetchResource.new("#{@organization}","project").perform
+      jira_projects = Jira::FetchResource.new(@organization.domain,"project").perform
 
       jira_projects.each do |jira_project|
-        project = Organization.find_by_domain(@organization).find_or_initialize_by(jira_project_id: jira_project[:id])
+        project = @organization.projects.find_or_initialize_by(jira_project_id: jira_project[:id])
 
         project.name = jira_project[:name]
         project.code = jira_project[:key]
